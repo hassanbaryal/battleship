e8e04b50ec184e6579bfedee4b0cb7d16d171bf4
hassanbaryal <h.baryal2@gmail.com>
2022-11-12 18:47:48 -0500

    Added tests for placeShip method of Gameboard object

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,6 +1,17 @@
 /* eslint-disable no-undef */
 import Gameboard from '../modules/Gameboard';
+import Ship from '../modules/Ship';
 
 describe('Testing Gameboard object', () => {
+  const carrier = Ship('Carrier', 5);
+  const destroyer = Ship('Destroyer', 2);
+  test.only('Places ship (recieves true)', () => {
+    const playerBoard = Gameboard();
+    expect(playerBoard.placeShip(carrier, [3, 2], 'down')).toBeTruthy();
+  });
 
+  test.only('Unable to place ship (recieves false)', () => {
+    const playerBoard = Gameboard();
+    expect(playerBoard.placeShip(carrier, [7, 2], 'right')).toBeTruthy();
+  });
 });
