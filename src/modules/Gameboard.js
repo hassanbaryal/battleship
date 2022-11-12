2eac937d8ac6cd69e5fde1dbb8c16489f93eb426
hassanbaryal <h.baryal2@gmail.com>
2022-11-12 18:48:01 -0500

    Added placeShip method

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,5 +1,30 @@
 const Gameboard = () => {
+  const cells = (() => {
+    const array = [];
+    for (let y = 0; y < 10; y += 1) {
+      const temp = [];
+      for (let x = 0; x < 10; x += 1) {
+        temp.push(null);
+      }
+      array.push(temp);
+    }
+    return array;
+  })();
 
+  const placeShip = (ship, coords, orientation) => {
+    let [x, y] = coords;
+    const coordinates = [];
+    for (let i = 0; i < ship.getLength(); i += 1) {
+      coordinates.push([x, y]);
+      cells[y][x] = ship;
+      if (orientation === 'right') x += 1;
+      else y += 1;
+      if (x > 9 || y > 9) return false;
+    }
+    return true;
+  };
+
+  return { placeShip };
 };
 
 export default Gameboard;
