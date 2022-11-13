c5474027fdf6f49c7e8bf2bb651460dcc5c459ab
hassanbaryal <h.baryal2@gmail.com>
2022-11-12 21:05:19 -0500

    Updated placeShip and added checkCell and removeShipFromMap method

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,5 +1,6 @@
 const Gameboard = () => {
-  const cells = (() => {
+  const shipsPlaced = [];
+  const map = (() => {
     const array = [];
     for (let y = 0; y < 10; y += 1) {
       const temp = [];
@@ -11,16 +12,43 @@ const Gameboard = () => {
     return array;
   })();
 
+  const checkCell = (coords) => {
+    const [x, y] = coords;
+    if (map[y][x] !== null) return false;
+    return true;
+  };
+
+  const removeShipFromMap = (ship) => {
+    for (let y = 0; y < map.length; y += 1) {
+      for (let x = 0; x < map.length; x += 1) {
+        if (map[y][x] === ship) map[y][x] = null;
+      }
+    }
+  };
+
   const placeShip = (ship, coords, orientation) => {
     let [x, y] = coords;
-    const coordinates = [];
+    const allCoordinates = [];
+    // For loop checks if cells are not occupied and
+    // are not outside map
     for (let i = 0; i < ship.getLength(); i += 1) {
-      coordinates.push([x, y]);
-      cells[y][x] = ship;
+      // If current cell is occupied, return false
+      if (!checkCell([x, y])) return false;
+      allCoordinates.push([x, y]);
       if (orientation === 'right') x += 1;
       else y += 1;
+      // If coordinates exceed map limits, return false
       if (x > 9 || y > 9) return false;
     }
+    // Remove previous location of ship, if ship is already placed
+    if (shipsPlaced.includes(ship)) removeShipFromMap(ship);
+
+    allCoordinates.forEach((coordinate) => {
+      map[coordinate[1]][coordinate[0]] = ship;
+    });
+
+    shipsPlaced.push(ship);
+
     return true;
   };
 
