41d8158ed999c22efbfbb8db132dd9db7137b096
hassanbaryal <h.baryal2@gmail.com>
2022-11-12 21:04:42 -0500

    Added more test cases for placeShip method

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -4,7 +4,7 @@ import Ship from '../modules/Ship';
 
 describe('Testing Gameboard object', () => {
   const carrier = Ship('Carrier', 5);
-  const destroyer = Ship('Destroyer', 2);
+  const carrierTwo = Ship('Destroyer', 5);
   test.only('Places ship (recieves true)', () => {
     const playerBoard = Gameboard();
     expect(playerBoard.placeShip(carrier, [3, 2], 'down')).toBeTruthy();
@@ -15,8 +15,16 @@ describe('Testing Gameboard object', () => {
     expect(playerBoard.placeShip(carrier, [7, 2], 'right')).toBeFalsy();
   });
 
-  // test.only('Unable to place ship (recieves false)', () => {
-  //   const playerBoard = Gameboard();
-  //   expect(playerBoard.placeShip(carrier, [7, 2], 'right')).toBeTruthy();
-  // });
+  test.only('Unable to place ship on occupied cell (recieves false)', () => {
+    const playerBoard = Gameboard();
+    playerBoard.placeShip(carrier, [7, 2], 'down');
+    expect(playerBoard.placeShip(carrier, [7, 2], 'down')).toBeFalsy();
+  });
+
+  test.only('Place ship at another location and remove previous location', () => {
+    const playerBoard = Gameboard();
+    playerBoard.placeShip(carrier, [7, 2], 'down');
+    playerBoard.placeShip(carrier, [1, 2], 'down');
+    expect(playerBoard.placeShip(carrierTwo, [7, 2], 'down')).toBeTruthy();
+  });
 });
