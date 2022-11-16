d4f1047d54f23581ae1100c2e17ae3d70f887f2e
hassanbaryal <h.baryal2@gmail.com>
2022-11-16 18:42:06 -0500

    Added coordinate and orientation properties, as well as getters and setters for the new properties

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -2,10 +2,20 @@ const Ship = (shipName, inputLength) => {
   const length = inputLength;
   const name = shipName;
   let hits = 0;
+  let coord = null;
+  let orientation = null;
 
   const getName = () => name;
 
   const getLength = () => length;
+
+  const getCoord = () => coord;
+
+  const getOrientation = () => orientation;
+
+  const setCoord = (newCoord) => { coord = newCoord; };
+
+  const setOrientation = (newOrient) => { orientation = newOrient; };
 
   // modify hit to take in coordinates and modify coordinates
   // object in array to be hit === true
@@ -20,7 +30,7 @@ const Ship = (shipName, inputLength) => {
   };
 
   return {
-    getName, getLength, hit, isSunk,
+    getName, getLength, getCoord, getOrientation, setCoord, setOrientation, hit, isSunk,
   };
 };
 
