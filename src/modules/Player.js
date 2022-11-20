69a212d738fb585c74a6429c98f3be73f2941706
hassanbaryal <h.baryal2@gmail.com>
2022-11-20 16:49:29 -0500

    Refaactored code

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -16,13 +16,17 @@ const Computer = (myBoard, ships) => {
   const name = possibleNames[Math.floor(Math.random() * possibleNames.length)];
   const board = myBoard;
 
+  // returns a random x,y coordinate
+  const getRandomCoords = () => [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+
   // Setup own board
   (() => {
     const orientations = ['right', 'down'];
     for (let i = 0; i < ships.length; i += 1) {
       let placed = false;
       while (!placed) {
-        const randomCoord = [Math.floor(Math.random * 10), Math.floor(Math.random * 10)];
+        // eslint-disable-next-line no-use-before-define
+        const randomCoord = getRandomCoords();
         const orientation = orientations[Math.floor(Math.random * 2)];
         // If ship place successful, update ship coords and orientation
         // exit while loop to move onto next ship
@@ -38,9 +42,6 @@ const Computer = (myBoard, ships) => {
   const getName = () => name;
 
   const getBoard = () => board;
-
-  // returns a random x,y coordinate
-  const getRandomCoords = () => [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
 
   const attack = (enemyBoard) => {
     // eslint-disable-next-line no-constant-condition
