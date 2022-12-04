475c2c8f5e8b9a0cc336b8a0922d2deb136419ea
hassanbaryal <h.baryal2@gmail.com>
2022-12-04 15:55:08 -0500

    UUpdated page

--- src/modules/endGame.js
+++ src/modules/endGame.js
@@ -6,10 +6,10 @@ const buildEndGamePage = (winner, loser, winnerMap, loserMap) => {
       <div class="end-game-msg"> ${winner.getName()} sent ${loser.getName()} to Davey Jones' Locker!</div>
       <div class="boards-container">
         <div class="player-one">
-          <div class="player-one-title">Winner: </div>
+          <div class="player-one-title">Winner: ${winner.getName()}</div>
         </div>
         <div class="player-two">
-          <div class="player-two-title">Loser: </div>
+          <div class="player-two-title">Loser: ${loser.getName()}</div>
         </div>
       </div>
       <button type="button" class="restart-btn">Restart</button>
