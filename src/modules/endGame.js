083f1206046be3072b2bc4b0081e357acf0492f8
hassanbaryal <h.baryal2@gmail.com>
2022-12-04 15:48:38 -0500

    Added function to create end game page

---
+++ src/modules/endGame.js
@@ -0,0 +1,28 @@
+import { removeMainContent, elementFromHtml } from './domFunctions';
+
+const buildEndGamePage = (winner, loser, winnerMap, loserMap) => {
+  const page = elementFromHtml(`
+    <div class="end-game-container">
+      <div class="end-game-msg"> ${winner.getName()} sent ${loser.getName()} to Davey Jones' Locker!</div>
+      <div class="boards-container">
+        <div class="player-one">
+          <div class="player-one-title">Winner: </div>
+        </div>
+        <div class="player-two">
+          <div class="player-two-title">Loser: </div>
+        </div>
+      </div>
+      <button type="button" class="restart-btn">Restart</button>
+    </div>
+  `);
+
+  const playerOneSection = page.querySelector('.player-one');
+  const playerTwoSection = page.querySelector('.player-two');
+
+  playerOneSection.appendChild(winnerMap.getVisibleMap());
+  playerTwoSection.appendChild(loserMap.getVisibleMap());
+
+  document.querySelector('main').appendChild(page);
+};
+
+export default buildEndGamePage;
