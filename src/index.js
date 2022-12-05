3040e95a8652297eb74cc028333738aacef83b95
hassanbaryal <h.baryal2@gmail.com>
2022-12-04 19:43:32 -0500

    Added functionality to toggle theme button

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import './styles/style.css';
 import './styles/mobile.css';
 import buildStartPage from './modules/startupPage';
 
+const root = document.documentElement;
+root.classList.toggle('light');
+
+document.querySelector('.toggle-theme-btn').addEventListener('click', () => {
+  root.classList.toggle('light');
+  root.classList.toggle('dark');
+});
+
 buildStartPage();
